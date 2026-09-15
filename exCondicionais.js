4922ab30e2c7ce1dd5fcd322c522b83b8128c60d
Fabricio Honorato Gomes <55056188808@info.local>
2026-09-15 16:20:01 -0300

    :iphone: feat: Exercícios para treinar JavaScript

--- exCondicionais.js
+++ exCondicionais.js
@@ -1,56 +1,123 @@
-// ------------------------------ REPETIÇÃO ------------------------------
+// ------------------------------ CONDICIONAIS ------------------------------
 
 //-------------------- NÍVEL BÁSICO --------------------
 
 //! 1---------- VERIFICAR MAIORIDADE ----------
+// let idade = 18;
 
+// if (idade < 18) {
+//     console.log("Você é menor de idade");
+// }
+// else {
+//     console.log("Você é maior de idade");
+// }
 
 //! 2---------- VERIFICAR SE UM NUMERO É POSITIVO OU NEGATIVO ----------
+// let numero = -1;
 
+// if (numero >= 0) {
+//     console.log("Este número é positivo");
+// } else {
+//     console.log("Este número é negativo");
+// }
 
 //! 3---------- APROVAÇÃO EM UMA PROVA ----------
+// let nota = 59;
 
+// if (nota >= 60) {
+//     console.log("Aluno(a) Aprovado(a)");
+// } else {
+//     console.log("Aluno(a) Reprovado(a)");
+// }
 
 //! 4---------- VERIFICAR SE UM NUMERO É POSITIVO, NEGATIVO OU ZERO ----------
+// let numero = 0
 
+// if (numero > 0) {
+//     console.log("O número é positivo");
+// } else if (numero < 0){
+//     console.log("O número é negativo");
+// }
+// else {
+//     console.log("O número é zero");
+// }
 
 //! 5---------- CLASSIFICAÇÃO DE IDADE ----------
-
+// let idade = 13;
+// if (idade <= 12) {
+//     console.log("Você é uma criança");
+// } else if(idade >= 13 && idade < 18){
+//     console.log("Você é um adolescente");
+// } else {
+//     console.log("Você é um adulto");
+// }
 
 //! 6---------- VERIFICAR SE UM NUMERO É PAR OU IMPAR ----------
+// let numero = 2;
 
+// if (numero % 2 == 0) {
+//     console.log("Este número é par");
+// } else {
+//     console.log("Este número é impar");
+// }
+//--------------------------------------------------------------------------------------------------------------------------------------------
 
 //-------------------- NÍVEL INTERMEDIÁRIO --------------------
 
-//! 1---------- EXIBIR OS NÚMEROS PARES DE 1 A 50 ----------
+//! 1---------- CALCULADORA SIMPLES ----------
+// let numero1 = 4;
+// let numero2 = 4;
+// let operacao = "*";
+
+// if (operacao == "+") {
+//     console.log(`A soma dos números resulta em: ${numero1 + numero2}`);
+// } else if(operacao == "-") {
+//     console.log(`A subtração dos números resulta em: ${numero1 - numero2}`);
+// } else if (operacao == "*") {
+//     console.log(`A multiplicação dos números resulta em: ${numero1 * numero2}`);
+// } else {
+//     console.log(`A divisão dos números resulta em: ${numero1 / numero2}`);
+// }
+
+//! 2---------- MAIOR ENTRE TRÊS NÚMEROS ----------
+// let numero1 = 8
+// let numero2 = 5
+// let numero3 = 7
+
+// if (numero1 > numero2 && numero1 > numero3) {
+//     console.log(`O número maior é: ${numero1}`);
+// } else if (numero2 > numero1 && numero2 > numero3) {
+//     console.log(`O número maior é: ${numero2}`);
+// } else {
+//     console.log(`O número maior é: ${numero3}`);
+// }
+
+//! 3---------- DESCONTO EM COMPRAS ----------
+let valorCompra = 100
+
+if (valorCompra > 100) {
+    console.log();
+    
+} else {
+    
+}
+
+//! 4---------- SISTEMA DE LOGIN SIMPLES ----------
 
 
-//! 2---------- JOGO DE ADIVINHAÇÃO ----------
-
-
-//! 3---------- CONTAGEM REGRESSIVA ----------
-
-
-//! 4---------- VALIDAÇÃO DE SENHA ----------
-
-
-//! 5---------- SOMA ATÉ PARAR ----------
-
-
-//! 6---------- MÉDIA DE VALORES ----------
-
+//--------------------------------------------------------------------------------------------------------------------------------------------
 //-------------------- NÍVEL AVANÇADO --------------------
 
-//! 1---------- SOMA DOS DÍGITOS DE UM NÚMERO ----------
+//! 1---------- FRENTE DE PEDIDO ----------
 
 
-//! 2---------- TABUADA DINÂMICA ----------
+//! 2---------- ACESSO POR IDADE E CONVITE ----------
 
 
-//! 3---------- INVERTER UM NÚMERO ----------
+//! 3---------- CONVERSÃO DE NOTAS PARA CONCEITOS ----------
 
 
-//! 4---------- MAIOR NÚMERO DIGITADO ----------
+//! 4---------- CÁLCULO DE IMC ----------
 
 
-//! 5---------- LISTA DE NOMES (ForEach) ----------
+//! 5---------- VERIFICAÇÃO DE ESTOQUE ----------
